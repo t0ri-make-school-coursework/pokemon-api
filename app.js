4d0067ac9e13051eda9c9941a411a9f3a83b78cc
t0ri <victoriarosemurray@gmail.com>
2019-01-30 14:52:30 -0800

    ya

--- app.js
+++ app.js
@@ -1,20 +1,54 @@
-// INDEX
-// app.get('/', (req, res) => {
-//   Trainer.find()
-//     .then(trainers => {
-//       res.render('trainers-index', { trainers: trainers });
-//     })
-//     .catch(err => {
-//       console.log(err);
-//     })
-// })
-//
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+//Server Start
+app.listen(port, () => {
+	console.log(`App listening on port ${port}`)
+})
+
+// get trainers
+app.get('/:id', (req, res) => {
+    res.json(json.trainers[req.params.id].name)
+})
 
 
-('Trainer', {
- Trainer_ID: Number,
- trainer_name: String,
- sex: ['Male','Female"],
- pokemon:[ 'Pikachu', 'Eevee','Charmander'],
- badges: ["Boulder Badge","Cascade Badge","Rainbow Badge"]
+
+let json = {
+    "trainers": [
+        {
+        "name":"tori",
+        "pokemon": [
+            "shinx",
+            "charmander",
+            "maractus",
+            "glaceon"]
+        },
+        {
+        "name": "anna",
+        "pokemon": [
+            "scyther",
+            "celebi",
+            "snorlax",
+            "ditto"]
+        },
+        {
+        "name": "anna",
+        "pokemon": [
+            "scyther",
+            "celebi",
+            "snorlax",
+            "ditto"]
+        }
+    ]
 }
+
+
+
+// ('Trainer', {
+//  Trainer_ID: Number,
+//  trainer_name: String,
+//  sex: ['Male','Female'],
+//  pokemon:[ 'Pikachu', 'Eevee','Charmander'],
+//  badges: ["Boulder Badge","Cascade Badge","Rainbow Badge"]
+// }
