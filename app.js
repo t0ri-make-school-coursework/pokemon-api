40eb89735d2b7a2e780f86eb489a16a6fa9134ab
MattHew Phraxayavong <matthewphraxayavong@MattHews-MacBook-Pro.local>
2019-01-30 14:26:31 -0800

    created trainer object

---
+++ app.js
@@ -0,0 +1,20 @@
+// INDEX
+// app.get('/', (req, res) => {
+//   Trainer.find()
+//     .then(trainers => {
+//       res.render('trainers-index', { trainers: trainers });
+//     })
+//     .catch(err => {
+//       console.log(err);
+//     })
+// })
+//
+
+
+('Trainer', {
+ Trainer_ID: Number,
+ trainer_name: String,
+ sex: ['Male','Female"],
+ pokemon:[ 'Pikachu', 'Eevee','Charmander'],
+ badges: ["Boulder Badge","Cascade Badge","Rainbow Badge"]
+}
